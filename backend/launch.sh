352aef893b49b0503573d40f21bf21152bf4d3c6
Kajeepan Kandeepan <kajeepan.kandeepan@gmail.com>
2023-03-10 18:26:21 +0000

    creation of superuser

--- backend/launch.sh
+++ backend/launch.sh
@@ -1,5 +1,8 @@
 source venv/bin/activate
 
+export $(grep -v '^#' ../.env | xargs)
+
 python3 manage.py makemigrations
 python3 manage.py migrate
+python3 manage.py createsuperuser --no-input
 python3 manage.py runserver 0:9000
